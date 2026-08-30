5b960e3451c49a76824884132eda20583ee17c09
Kanishka Mendis <kmadusanka1@gmail.com>
2026-08-30 12:28:38 +0530

    Initial Commit

---
+++ BatchBreak/BatchBreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatchBreakApp.swift
+//  BatchBreak
+//
+//  Created by Kanishka Madusanka on 2026-08-30.
+//
+
+import SwiftUI
+
+@main
+struct BatchBreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BatchBreak/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BatchBreak
+//
+//  Created by Kanishka Madusanka on 2026-08-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
